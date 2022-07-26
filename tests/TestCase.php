7d10ca57e451c81177d63820f70a40463c22da67
Ekojoka Chris Idakwo <chris.idakwo@gmail.com>
2022-07-27 00:38:16 +0100

    Start out tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,23 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Artisan::call('migrate --seed');
+    }
+
+    protected function tearDown(): void
+    {
+        Artisan::call('migrate:reset');
+
+        parent::tearDown();
+    }
 }
---
+++ tests/Unit/ResourceServiceTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Resource;
+use App\Services\Contracts\ResourceService;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ResourceServiceTest extends TestCase
+{
+    use DatabaseMigrations, WithFaker;
+
+    protected ResourceService $resourceService;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->resourceService = resolve(ResourceService::class);
+    }
+
+    public function testThatItGetsResources(): void
+    {
+        $resources = $this->resourceService->listResources(1, 10);
+
+        $data = $resources->getCollection()->toArray();
+
+        self::assertCount(10, $data);
+        self::assertEquals(10, $resources->perPage());
+    }
+
+    public function testItCreatesNewResource(): void
+    {
+        $data = [
+            'type' => Resource::TYPE_LINK,
+            'title' => $this->faker->text(50),
+            'link' => $this->faker->url,
+            'link_target' => '_blank',
+        ];
+
+        $resource = $this->resourceService->createResource($data['type'], $data);
+
+        self::assertInstanceOf(Resource::class, $resource);
+        self::assertEquals('link', $resource->type);
+        self::assertEquals('_blank', $resource->link_target);
+    }
+}
